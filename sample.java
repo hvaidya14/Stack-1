1e4236500ea1e2867ea6b86032464033ab8215bd
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-03-05 15:34:01 -0800

    Update sample.java

--- sample.java
+++ sample.java
@@ -5,3 +5,18 @@
 
 
 // Your code here along with comments explaining your approach
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        Stack<Integer> s = new Stack<>();
+        s.push(0);
+        for (int i=1;i<temperatures.length;i++) {
+            while(!s.isEmpty() && temperatures[i] > temperatures[s.peek()]) {
+                int popped = s.pop();
+                result[popped] = i-popped;
+            }
+            s.push(i);
+        }
+        return result;
+    }
+}
